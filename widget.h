3b418b6f6c2a1259ad292f07104ecbdb5e8732dc
combofish <combofish@139.com>
2022-03-29 12:56:36 +0800

    Init commit.

---
+++ widget.h
@@ -0,0 +1,37 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTimer>
+#include <QClipboard>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+    void format_string(QString &, QString &);
+
+private slots:
+    void on_checkbox_enable_formatting_stateChanged(int arg1);
+
+    void on_checkBox_auto_copy_stateChanged(int arg1);
+
+    void timer_update();
+
+private:
+    Ui::Widget *ui;
+    QTimer *timer;
+    QClipboard *clipboard;
+    QString origin_str;
+    QString formatted_str;
+
+
+};
+#endif // WIDGET_H
